1bf600feeffb0e6d8c3561a23e1cc4b1b9f79961
swathisree <swathitharmal@gmail.com>
2021-06-13 09:14:44 -0800

    all files

---
+++ javascript/flowcontrols/decisionmaking/largest2.js
@@ -0,0 +1,13 @@
+var num1=3
+var num2=8
+console.log("Numbers are "+num1+" and "+num2)
+
+
+if(num1>num2)
+console.log(num1)
+
+else if(num2>num1)
+console.log(num2)
+
+else
+console.log("both are equal")
---
+++ javascript/flowcontrols/decisionmaking/negativepositive.js
@@ -0,0 +1,11 @@
+var num=0
+console.log("Number is "+num)
+
+if(num>0)
+console.log("num is positive")
+
+else if(num<0)
+console.log("num is negative")
+
+else
+console.log("num is zero")
